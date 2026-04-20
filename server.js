b326baf3c01948a43c0894c7ea23de85413a3a27
wittns <mugishawitness15@gmail.com>
2026-04-20 13:54:04 +0200

    client messaging

--- server.js
+++ server.js
@@ -2,14 +2,32 @@ import dotenv from "dotenv"
 dotenv.config()
 import {fileURLToPath} from "url"
 import path from "path"
+import http from "http"
 import express from "express"
+import {Server} from "socket.io"
 const app = express()
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"publice")))
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,()=>{
+const server = http.createServer(app)
+
+const io = new Server(server);
+io.on("connection",(socket)=>{
+    // welcome message
+    socket.emit("message","Welcome on ChartCord!")
+
+    // when a new user connects
+    socket.broadcast.emit("message","A new user has joined the chart")
+
+    //when a user disconnects 
+     socket.on("disconnect",()=>{
+      io.emit("message","user left chartCord")
+            })  
+})
+
+server.listen(PORT,()=>{
     console.log(`server running on PORT:${PORT}`)
 })
